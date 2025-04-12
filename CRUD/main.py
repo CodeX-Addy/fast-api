093b832d3800fd36eb24142f80b742ae98e0fc19
CodeX-Addy <aditya.tomar9055049@gmail.com>
2025-04-13 00:03:28 +0530

    add: TBC

---
+++ CRUD/main.py
@@ -0,0 +1,23 @@
+## Imports for FastAPI
+
+import os
+from dotenv import load_dotenv
+from fastapi import FastAPI, HTTPException, Depends
+from pydantic import BaseModel
+from sqlalchemy import Integer, String, create_engine, Column
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker, Session
+
+load_dotenv()
+
+DATABASE_URL = os.environ.get("DATABASE_URL")
+engine = create_engine(DATABASE_URL)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+Base = declarative_base()
+
+class NameModel(Base):
+    __tablename__ = "names"
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, index=True)
+
+Base.metadata.create_all(bind=engine)
