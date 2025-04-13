5281d2a9e1e36447bb233f693695fc9a7b9dec0c
CodeX-Addy <aditya.tomar9055049@gmail.com>
2025-04-13 23:41:03 +0530

    add: app

--- CRUD/main.py
+++ CRUD/main.py
@@ -13,9 +13,35 @@ engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
+## ----------------Model------------------------
 class NameModel(Base):
     __tablename__ = "names"
     id = Column(Integer, primary_key=True, index=True)
     name = Column(String, index=True)
 
 Base.metadata.create_all(bind=engine)
+
+## ----------------Pydantic Model Schema------------------------
+
+class NameCreate(BaseModel):
+    name: str
+
+class NameResponse(BaseModel):
+    id:int
+    name: str
+
+    class Config:
+        orm_mode = True
+
+## ----------------FastAPI App------------------------
+
+app = FastAPI()
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+## ----------------CRUD ROUTES Endpoints------------------------
